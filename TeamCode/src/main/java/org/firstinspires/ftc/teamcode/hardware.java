5ffd201c660d69876cff13b83b9511bd59f48cfc
bobbykabob <73088639+bobbykabob@users.noreply.github.com>
2021-11-08 14:34:22 -0800

    Update hardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.intake;
+import org.firstinspires.ftc.teamcode.outtake;
 
 public hardware() {
   
+  public intake intake;
+  public outtake outtake;
   private hardwareMap hw;
   public void hardware(hardwareMap ahw) {
     hw = ahw;
-    
-  }
+    intake = new intake(hw);
+    outtake = new outtake(hw);
+  } 
 }
