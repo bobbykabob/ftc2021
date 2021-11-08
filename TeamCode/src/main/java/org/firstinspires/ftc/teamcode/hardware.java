c316b3ae6fbeb4456d6f6bea8e019dac6766cd15
bobbykabob <73088639+bobbykabob@users.noreply.github.com>
2021-11-08 14:27:37 -0800

    Create hardware.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public hardware() {
+  
+  private hardwareMap hw;
+  public void hardware(hardwareMap ahw) {
+    hw = ahw;
+    
+  }
+}
